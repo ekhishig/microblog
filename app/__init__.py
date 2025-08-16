afdf1bfd9e6f20db00fc9b0d207da4a5a786940b
ekhishig <khishigmaa.enkhtur@gmail.com>
2025-08-16 08:20:51 +0200

    Falsk-Login extension is used to manage user sessions and authentication.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config) # Load configuration from the Config class
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
 
-from app import routes  # Import routes to register them with the Flask app
+from app import routes, models  
--- microblog.py
+++ microblog.py
@@ -1,1 +1,15 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    """Provide a shell context for Flask's shell command."""
+    return {
+        'sa': sa,
+        'so': so,
+        'db': db,
+        'User': User,
+        'Post': Post
+    }
